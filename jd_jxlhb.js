5b78226abb5062b146a5e11ccf2f67df181f6ccd
“dreamStart” <Liang82725568>
2021-08-18 14:41:08 +0800

    update

--- jd_jxlhb.js
+++ jd_jxlhb.js
@@ -86,6 +86,7 @@ const BASE_URL = 'https://wq.jd.com/cubeactive/steprewardv3'
       if ($.UserName === code['userName']) continue;
       console.log(`【${$.UserName}】去助力【${code['userName']}】邀请码：${code['strUserPin']}`);
       await enrollFriend(code['strUserPin']);
+      await $.wait(3000);
       await $.wait(2000);
       if ($.max) continue
       if (!$.canHelp) break
@@ -96,6 +97,7 @@ const BASE_URL = 'https://wq.jd.com/cubeactive/steprewardv3'
         if (!item) continue;
         console.log(`【${$.UserName}】去助力作者的邀请码：${item}`);
         await enrollFriend(item);
+        await $.wait(3000);
         await $.wait(2000);
         if ($.max) continue
         if (!$.canHelp) break
@@ -126,6 +128,7 @@ async function main() {
   await joinActive();
   await $.wait(2000)
   await getUserInfo()
+  await $.wait(2000)
 }
 //参与活动
 function joinActive() {
